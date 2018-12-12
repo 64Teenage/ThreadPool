e67a97ba4b2c801aff131fb737710abb8b75c122
64Teenaage <hqhwhuer@outlook.com>
2018-12-12 19:20:44 +0800

    modify tcpClient.cpp

--- test/tcpClient.cpp
+++ test/tcpClient.cpp
@@ -21,7 +21,7 @@ int main(int argc, char ** argv)
 
     if (argc != 3)
     {
-        printf("usage : tcpClient <ip> <path>");
+        printf("usage : tcpClient <ip> <path>\n");
         exit(0);
     }
 
